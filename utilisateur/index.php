9d845b7d588d83a45ad6c0fb10852a87d5e9b6bd
Joël Troch <joel.troch62@gmail.com>
2014-10-06 17:24:04 +0200

    Page d'accueil de la section utilisateur : bouton "Se déconnecter" ajouté, titre changé et texte de présentation ajouté.

--- utilisateur/index.php
+++ utilisateur/index.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="description" content="Site intranet pour la réservation des salles de la Maison des Ligues de Lorraine" />
 		<meta name="author" content="netEmedia" />
-		<title>Site intranet M2L - Réservation des salles</title>
+		<title>Site intranet M2L - Accueil utilisateur</title>
 		<link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
 		<link href="../css/bootstrap-theme-colors.css" rel="stylesheet" type="text/css" media="all" />
 		<link href="../css/theme.css" rel="stylesheet" type="text/css" media="all" />
@@ -19,7 +19,7 @@
 			<div class="container">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="sr-only">Afficher/Masquer menu de navigation</span>
+						<span class="sr-only">Afficher/masquer menu de navigation</span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
@@ -29,16 +29,22 @@
 				<div class="navbar-collapse collapse">
          			<ul class="nav navbar-nav">
             			<li class="active"><a href="#">Accueil</a></li>
-            			<li><a href="#">Salles</a></li>
+            			<li><a href="salles.php">Salles</a></li>
             			<li><a href="#">Réservations</a></li>
+          			</ul>
+          			<ul class="nav navbar-nav navbar-right">
+          				<li class="active"><a href="../index.php">Se déconnecter</a></li>
           			</ul>
         		</div>
 			</div>
 		</div>
 		<div class="container theme-showcase" role="main">
 			<div>
-				<h1>M2L</h1>
-				<p class="lead">Hello World !</p>
+				<h1>Accueil</h1>
+				<p class="lead">Bienvenue sur la page d'accueil de la section utilisateur du site intranet.</p>
+				<p class="lead">Pour consulter les informations sur les salles, cliquez sur le bouton "Salles" dans le menu en haut de l'écran.</p>
+				<p class="lead">Les informations sur les réservations sont disponible en cliquant sur le bouton "Réservations" dans le même menu.</p>
+				<p class="lead">Vous pouvez vous déconnecter à tout moment en cliquant sur le bouton prévu à cet effet en haut à droite.</p>
 			</div>
 		</div>
 		<script type="text/javascript" src="../js/jquery-1.11.1.js"></script>
