67c96fe105e2756c88902cc822ad3f3d06a5165f
Joël Troch <joel.troch62@gmail.com>
2014-10-06 14:38:43 +0200

    Début page d'accueil utilisateur

---
+++ utilisateur/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content="Site intranet pour la réservation des salles de la Maison des Ligues de Lorraine" />
+		<meta name="author" content="netEmedia" />
+		<title>Site intranet M2L - Réservation des salles</title>
+		<link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+		<link href="../css/bootstrap-theme-colors.css" rel="stylesheet" type="text/css" media="all" />
+		<link href="../css/theme.css" rel="stylesheet" type="text/css" media="all" />
+		<!--[if lt IE 9]>
+			<script type="text/javascript" src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script type="text/javascript" src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body role="document">
+		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Afficher/Masquer menu de navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<p><a class="navbar-brand" href="#">M2L - Réservation des salles</a></p>
+				</div>
+				<div class="navbar-collapse collapse">
+         			<ul class="nav navbar-nav">
+            			<li class="active"><a href="#">Accueil</a></li>
+            			<li><a href="#">Salles</a></li>
+            			<li><a href="#">Réservations</a></li>
+          			</ul>
+        		</div>
+			</div>
+		</div>
+		<div class="container theme-showcase" role="main">
+			<div>
+				<h1>M2L</h1>
+				<p class="lead">Hello World !</p>
+			</div>
+		</div>
+		<script type="text/javascript" src="../js/jquery-1.11.1.js"></script>
+		<script type="text/javascript" src="../js/bootstrap.js"></script>
+	</body>
+</html>
