264943c344563ae25b6f0e7301e7883e6cd6a9a5
Maxime-s <seux.maxime@gmail.com>
2014-10-13 16:07:28 +0200

    Refonte navbar de toutes les pages et des comboboxs.Ajout du champ date dans réservations.

--- administrateur/index.php
+++ administrateur/index.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="description" content="Site intranet pour la réservation des salles de la Maison des Ligues de Lorraine" />
 		<meta name="author" content="netEmedia" />
-		<title>Site intranet M2L - Acceuil Administrateur</title>
+		<title>Site intranet M2L - Acceuil Administration</title>
 		<link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
 		<link href="../css/bootstrap-theme-colors.css" rel="stylesheet" type="text/css" media="all" />
 		<link href="../css/theme.css" rel="stylesheet" type="text/css" media="all" />
@@ -15,16 +15,31 @@
 		<![endif]-->
 	</head>
 	<body role="document">
-		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-			<div class="container">
-				<div class="navbar-header">
-					<p class="navbar-brand">M2L - Réservation des salles</p>
-                                        <p class="navbar-brand"><a href="../administrateur/utilisateurs.php">Utilisateurs</a></p>
-                                        <p class="navbar-brand"><a href="../administrateur/salles.php">Salles</a></p>
-                                        <p class="navbar-brand"><a href="../administrateur/reservations.php">Réservations</a></p>
-				</div>
-			</div>
-		</div>
+	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Afficher/masquer menu de navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+			</button>
+			<p><a class="navbar-brand" href="#">M2L - Acceuil administration</a></p>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="index.php">Accueil</a></li>
+                            <li><a href="utilisateurs.php">Utilisateurs</a></li>
+                            <li><a href="salles.php">Salles</a></li>
+                            <li><a href="reservations.php">Réservations</a></li>
+          		</ul>
+          		<ul class="nav navbar-nav navbar-right">
+                            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Bruce Wayne (Administrateur)</a></li>
+                            <li class="active"><a href="../index.php">Se déconnecter</a></li>
+          		</ul>
+                    </div>
+                </div>
+            </div>
 		<div class="container theme-showcase" role="main">
 			<div>
 				<h1>M2L</h1>
--- administrateur/reservations.php
+++ administrateur/reservations.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="description" content="Site intranet pour la réservation des salles de la Maison des Ligues de Lorraine" />
 		<meta name="author" content="netEmedia" />
-		<title>Site intranet M2L - Administrateur Réservations</title>
+		<title>Site intranet M2L - Administration réservations</title>
 		<link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
 		<link href="../css/bootstrap-theme-colors.css" rel="stylesheet" type="text/css" media="all" />
 		<link href="../css/theme.css" rel="stylesheet" type="text/css" media="all" />
@@ -15,21 +15,56 @@
 		<![endif]-->
 	</head>
 	<body role="document">
-		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-			<div class="container">
-				<div class="navbar-header">
-					<p class="navbar-brand">M2L - Réservation des salles</p>
-                                        <p class="navbar-brand"><a href="../administrateur/utilisateurs.php">Utilisateurs</a></p>
-                                        <p class="navbar-brand"><a href="../administrateur/salles.php">Salles</a></p>
-                                        <p class="navbar-brand"><a href="../administrateur/reservations.php">Réservations</a></p>
-				</div>
-			</div>
-		</div>
+            <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Afficher/masquer menu de navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+			</button>
+			<p><a class="navbar-brand" href="#">M2L - Administration réservations</a></p>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li><a href="index.php">Accueil</a></li>
+                            <li><a href="utilisateurs.php">Utilisateurs</a></li>
+                            <li><a href="salles.php">Salles</a></li>
+                            <li class="active"><a href="reservations.php">Réservations</a></li>
+          		</ul>
+          		<ul class="nav navbar-nav navbar-right">
+                            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Bruce Wayne (Administrateur)</a></li>
+                            <li class="active"><a href="../index.php">Se déconnecter</a></li>
+          		</ul>
+                    </div>
+                </div>
+            </div>
 		<div class="container theme-showcase" role="main">
-			<div>
-				<h1>M2L</h1>
-				<p class="lead">Page réservations</p>
-			</div>
+                    <div class="control-group">
+                        <label class="control-label"for="salles">
+                            Choix de la salle :
+                        </label>
+                        <select name="salles" class="form-control">
+                            <option value="0"></option>
+                            <option value="1">Salle 1</option>
+                            <option value="2">Salle 2</option>
+                            <option value="3">Salle 3</option>
+                        </select>
+                        <br/>
+                        <label class="control-label"for="utilisateurs">
+                            Choix de l'utilisateur :
+                        </label>
+                        <select name="utilisateurs" class="form-control">
+                            <option value="0"></option>
+                            <option value="1">Marion Martin</option>
+                            <option value="2">Gérard Gégé</option>
+                            <option value="3">Luc Lamaison</option>
+                        </select>
+                        <br/>
+                        <label for="champ1">Choix du jour :</label>
+                        <input type="date" id="ChoixDuJour" name="jour">
+                    </div>
 		</div>
 		<script type="text/javascript" src="../js/jquery-1.11.1.js"></script>
 		<script type="text/javascript" src="../js/bootstrap.js"></script>
